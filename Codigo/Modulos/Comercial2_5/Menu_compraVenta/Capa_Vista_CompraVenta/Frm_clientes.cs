917458cf004cc502e9d0b881f18d17474da5f8ed
JOSUE DAVID PAZ GOMEZ <jpazg10@miumg.edu.gt>
2025-06-05 09:44:22 -0600

    Ramacontenedor

--- Codigo/Modulos/Comercial2_5/Menu_compraVenta/Capa_Vista_CompraVenta/Frm_clientes.cs
+++ Codigo/Modulos/Comercial2_5/Menu_compraVenta/Capa_Vista_CompraVenta/Frm_clientes.cs
@@ -25,7 +25,12 @@ namespace Capa_Vista_CompraVenta
             navegador1.ObtenerIdAplicacion("3000");
             navegador1.ObtenerIdUsuario(idUsuario);
             navegador1.AsignarAyuda("1");
-            navegador1.AsignarNombreForm("Mantenimiento clientes");
+            navegador1.AsignarNombreForm("Mantenimiento Carreras");
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
